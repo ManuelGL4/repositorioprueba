2579029c4971f28eaad9155264b695aa03b000d4
b14-12t <b14-12t@AULAB14-PC12.CICLOS.local>
2023-03-02 19:58:02 +0100

    prueba

--- ENDES_GitHub/src/ClaseUno.java
+++ ENDES_GitHub/src/ClaseUno.java
@@ -3,6 +3,7 @@ public class ClaseUno {
 
 	public static void main(String[] args) {
 		System.out.println("GitHub ENDES Clase dos");
+		System.out.println("Prueba de que va en la otra clase");
 	}
 
 }
