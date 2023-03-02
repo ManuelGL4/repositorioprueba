c1d962e408a4f0dc577e508b16a5ec9242642dc4
b14-12t <b14-12t@AULAB14-PC12.CICLOS.local>
2023-03-02 19:44:14 +0100

    Primer commit

---
+++ ENDES_GitHub/src/ClaseDos.java
@@ -0,0 +1,9 @@
+
+public class ClaseDos {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub ENDES Clase dos");
+
+	}
+
+}
---
+++ ENDES_GitHub/src/ClaseUno.java
@@ -0,0 +1,8 @@
+
+public class ClaseUno {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub ENDES Clase dos");
+	}
+
+}
